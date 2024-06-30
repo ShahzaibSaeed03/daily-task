8c5935ac63f90acfc8236c7822604ec36044e07e
Shahzaib Saeed <115952045+ShahzaibSaeed03@users.noreply.github.com>
2024-06-30 12:09:34 +0500

    Update json-server.js

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,32 +1,37 @@
+const { createServer } = require('http');
+const { parse } = require('url');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./netlify/functions/db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const handler = async (event, context) => {
-  const { path } = event;
-  const response = await new Promise((resolve) => {
-    server.handle({ url: path, method: event.httpMethod }, {
-      setHeader: (key, value) => {
-        if (!response.headers) {
-          response.headers = {};
+const app = createServer((req, res) => {
+  const url = parse(req.url, true);
+  if (url.pathname.startsWith('/.netlify/functions/json-server')) {
+    req.url = req.url.replace('/.netlify/functions/json-server', '');
+    server(req, res);
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(9000, () => {
+  console.log('JSON Server is running on port 9000');
+});
+
+exports.handler = (event, context) => {
+  return new Promise((resolve, reject) => {
+    app.emit('request', event, {
+      end: resolve,
+      setHeader: (name, value) => {
+        if (name === 'content-type') {
+          context.headers[name.toLowerCase()] = value;
         }
-        response.headers[key] = value;
-      },
-      end: (body) => {
-        response.body = body;
-        resolve(response);
       }
     });
   });
-  return {
-    statusCode: response.statusCode || 200,
-    body: response.body,
-    headers: response.headers,
-  };
 };
-
-module.exports = { handler };
